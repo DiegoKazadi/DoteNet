ef1f8c1b662a9c846d073231c03208b2d367e60a
Diego Kazadi <diego.kalunda@maisunifacisa.com.br>
2020-04-16 15:25:43 -0300

    alterações no procedimento

--- AppPacientes/AppPacientes/Models/Procedimento.cs
+++ AppPacientes/AppPacientes/Models/Procedimento.cs
@@ -7,21 +7,8 @@ namespace AppPacientes.Models
 {
     public class Procedimento
     {
-
-        [Key]
         public int id { get; set; }
-        [Required(ErrorMessage = "O nome do procedimento é obrigatório", AllowEmptyStrings = false)]
-        [Display(Name="Nome")]
-       
+
         public string Nome { get; set; }
-
-        public List<Procedimento> ListaProcedimento()
-        {
-            return new List<Procedimento>
-
-            {
-                new Procedimento{ id = 1, Nome = "Clariamento"}
-            };
-        }
     }
 }
