775f8bc4e63bea8a27aa109616a58e7658141c25
Diego Kazadi <diego.kalunda@maisunifacisa.com.br>
2020-06-17 15:19:48 -0300

    atulizando

---
+++ AppAcoes/AppAcoes/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using AppAcoes.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AppAcoes.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Acoes> Acoes { get; set; }
+
+    }
+}
---
+++ AppAcoes/AppAcoes/Models/Acoes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppAcoes.Models
+{
+    public class Acoes
+    {
+        public Guid Id { get; set; }
+        public string codigoAcao { get; set; }
+        public int quantdAcao { get; set; }
+        public double precoAcao { get; set; }
+        public string data { get; set; }
+    }
+}
